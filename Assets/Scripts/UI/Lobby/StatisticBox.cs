0e78f2b12fd244bbb823557cd2e2fbad6560b771
Michal Karcz <michal.karcz.00@gmail.com>
2020-09-05 07:22:23 +0200

    tegesy

---
+++ Assets/Scripts/UI/Lobby/StatisticBox.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatisticBox : MonoBehaviour
+{
+    [SerializeField] private TMPro.TextMeshProUGUI _nameText;
+    [SerializeField] private TMPro.TextMeshProUGUI _valueText;
+
+    [Space]
+    [SerializeField] private TMPro.TextMeshProUGUI _upgradeCostText;
+    [SerializeField] private Button _upgradeButton;
+
+    public System.Action OnUpgrade;
+
+    public void Init(string name, string value, string upgradeCost)
+    {
+        _nameText.text = name;
+        _valueText.text = value;
+        _upgradeCostText.text = upgradeCost;
+    }
+
+    private void Start()
+    {
+        _upgradeButton.onClick.AddListener(Upgrade);
+    }
+
+    public void Upgrade()
+    {
+        OnUpgrade?.Invoke();
+    }
+}
