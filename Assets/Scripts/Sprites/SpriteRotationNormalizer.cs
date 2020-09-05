f205c0d482080f8efd5b1e4b612a8f75044947b2
Łukasz Mółka <lukasz.molka5@gmail.com>
2020-09-05 03:52:40 +0200

    Sprajty, kolory, postprocesy

---
+++ Assets/Scripts/Sprites/SpriteRotationNormalizer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRotationNormalizer : MonoBehaviour
+{
+    [SerializeField] private Vector3 _rotation;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(_rotation);
+    }
+}
