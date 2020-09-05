398941607d60a75acb969f1b316af84e11af5c9f
pitfon <54739288+pitfon@users.noreply.github.com>
2020-09-05 02:16:47 +0200

    wstępne strzelanie

---
+++ Assets/scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private KeyCode shoot;
+    public float speed=15;
+    [SerializeField]
+    PlayerController controller;
+    GameObject character;
+    private Vector2 direction;
+    private void Awake()
+    {
+        character = GameObject.Find("Character");
+        controller = character.GetComponent<PlayerController>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(shoot))
+        {
+            direction = controller.direction;
+        }
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
