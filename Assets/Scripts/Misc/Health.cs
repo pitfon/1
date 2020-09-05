ec7badd80cae1f8e620100a53da1cb7a98020f41
pitfon <54739288+pitfon@users.noreply.github.com>
2020-09-05 10:26:22 +0200

    poprawki

--- Assets/Scripts/Misc/Health.cs
+++ Assets/Scripts/Misc/Health.cs
@@ -5,24 +5,34 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer _spriteRenderer;
+    public float CurrentHealth { get; private set; } = 100;
 
-    protected int _currentHealth = 100;
+    public System.Action OnCurrentHealthChanged;
 
     protected virtual void Start()
     {
         
     }
-
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == ("Bullet"))
+        {
+            print("hit");
+            Damage(25);
+        }
+    }
     protected virtual void Update()
     {
         if (Input.GetMouseButtonDown(0)) Damage(25);
     }
-
     public virtual void Damage(int damage)
     {
-        _currentHealth -= damage;
+        CurrentHealth -= damage;
         StartCoroutine(DamageFlash());
-        if(_currentHealth <= 0)
+        print(CurrentHealth);
+        OnCurrentHealthChanged?.Invoke();
+        
+        if(CurrentHealth <= 0)
         {
             Death();
         }
@@ -48,4 +58,6 @@ public class Health : MonoBehaviour
     {
         gameObject.SetActive(false);
     }
+
+    
 }
