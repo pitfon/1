3cad50d828ba0d4e13ddbaf3636adde133e842a2
Łukasz Mółka <lukasz.molka5@gmail.com>
2020-09-05 15:42:30 +0200

    pan sabotasz

--- Assets/Scripts/AI/AI_Base.cs
+++ Assets/Scripts/AI/AI_Base.cs
@@ -6,18 +6,39 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public abstract class AI_Base : MonoBehaviour
 {
-    [SerializeField] private Transform _target;
-    NavMeshAgent _agent;
+    [SerializeField] protected float _attackRange;
+    [SerializeField] protected float _attackCooldown = 1.0f;
+    [SerializeField] protected float _attackDamage;
+    [SerializeField] protected Transform _target;
+
+    protected float _attackTimer;
+    protected NavMeshAgent _agent;
 
 
     protected virtual void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
+
     }
 
     protected virtual void Update()
     {
-        //temp
+        //temp   
         _agent.SetDestination(_target.position);
+
+        if (_agent.remainingDistance <= _attackRange && _agent.remainingDistance > 0)
+        {
+            if (_attackTimer <= 0)
+            {
+                Attack();
+                _attackTimer = _attackCooldown;
+            }
+        }
+        _attackTimer -= Time.deltaTime;
+    }
+
+    protected virtual void Attack()
+    {
+
     }
 }
---
+++ Assets/Scripts/Misc/Destroyer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    [SerializeField] private float _lifetime = 1.0f;
+    void Start()
+    {
+        StartCoroutine(DestroyThis());
+    }
+
+    IEnumerator DestroyThis()
+    {
+        float timer = 0.0f;
+        while(timer < _lifetime)
+        {
+            yield return new WaitForEndOfFrame();
+            timer += Time.deltaTime;
+        }
+        Destroy(this);
+    }
+}
