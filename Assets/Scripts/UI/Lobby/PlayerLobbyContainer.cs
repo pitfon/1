a0a6ac50e26831bbbc8e1393d3f9ba4867ae5254
Michal Karcz <michal.karcz.00@gmail.com>
2020-09-05 06:14:23 +0200

    lobby

---
+++ Assets/Scripts/UI/Lobby/PlayerLobbyContainer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLobbyContainer : MonoBehaviour
+{
+    [SerializeField] private PlayerInfoLobbyView _playerInfoLobbyView;
+
+    public void Init(PlayerData data)
+    {
+        _playerInfoLobbyView.Init(data);
+    }
+}
