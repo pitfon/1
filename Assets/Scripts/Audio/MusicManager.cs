60816e5e43b53c55ae21d9f16fc3513902bd37cd
Łukasz Mółka <lukasz.molka5@gmail.com>
2020-09-06 12:26:09 +0200

    auuuudiio

---
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public bool IsBossBattle;
+
+    [SerializeField] private AudioClip _normalMusic, _bossMusic;
+    [SerializeField] private AudioSource _audioSource;
+
+    void Start()
+    {
+        AudioClip clip = (IsBossBattle) ? _bossMusic : _normalMusic;
+        _audioSource.clip = clip;
+        _audioSource.Play();
+    }
+}
