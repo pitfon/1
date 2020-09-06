498efb53ad808d0b42782f1d43d78cdaed710e5a
pitfon <54739288+pitfon@users.noreply.github.com>
2020-09-06 15:19:50 +0200

    apteczka

---
+++ Assets/scripts/Items/HealBag.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealBag : ItemObject
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Health health = other.GetComponent<Health>();
+            if (health.CurrentHealth < health.MaxHealth)
+            {
+                health.Heal((int)item.Value);
+
+                Destroy(gameObject);
+            }
+        }
+    }
+
+
+}
+public class ItemObject : MonoBehaviour{
+    public Item item;
+
+}
